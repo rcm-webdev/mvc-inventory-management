7b71e7a135a665d0a5d5b045cc3819777e60f231
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-11-29 14:05:39 -0800

    configure todos routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const todosController = require('../controllers/todos') 
-const { ensureAuth } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const todosController = require("../controllers/todos");
+const { ensureAuth } = require("../middleware/auth");
 
-router.get('/', ensureAuth, todosController.getTodos)
+router.get("/", ensureAuth, todosController.getTodos);
 
-router.post('/createTodo', todosController.createTodo)
+router.post("/createTodo", upload.single("file"), todosController.createTodo);
 
-router.put('/markComplete', todosController.markComplete)
+router.put("/markComplete", todosController.markComplete);
 
-router.put('/markIncomplete', todosController.markIncomplete)
+router.put("/markIncomplete", todosController.markIncomplete);
 
-router.delete('/deleteTodo', todosController.deleteTodo)
+router.delete("/deleteTodo", todosController.deleteTodo);
 
-module.exports = router
+module.exports = router;
